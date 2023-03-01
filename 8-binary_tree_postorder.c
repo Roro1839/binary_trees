f0f13188d5e0d3ab139aadab4a9306ee4c1af5c3
njogued <njogued@gmail.com>
2023-03-01 02:01:52 -0800

    Corrected

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - print tree values in-order
+ * binary_tree_postorder - print tree values post-order
  * @tree: The tree to iterate through
  * @func: Function pointer for print function
  * Return: Nothing
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	binary_tree_inorder(tree->left, func);
-	binary_tree_inorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
