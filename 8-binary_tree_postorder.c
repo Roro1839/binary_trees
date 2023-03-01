1900dccc16ae05a4cae7e5ad2ac862481db45594
njogued <njogued@gmail.com>
2023-03-01 02:10:26 -0800

    Postorder

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - print tree values post-order
+ * @tree: The tree to iterate through
+ * @func: Function pointer for print function
+ * Return: Nothing
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
