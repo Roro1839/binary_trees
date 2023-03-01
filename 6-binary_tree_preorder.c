d186e6565534e135cfbe6dd6ec43a52fd52864be
ed <njogued@gmail.com>
2023-03-01 12:56:20 +0300

    Pull Request #1 (#1)
    * Preorder Traversal
    
    * Traversal
    
    * Traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - Preorder traversal
+ * @tree: Binary tree tree
+ * @func: Function to print the number
+ * Return: Nothing
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func == NULL || tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
